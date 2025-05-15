f21eca7a78bf605be0964c4a66db2a769794e841
Estefanio N Santos <estefanions@gmail.com>
2025-05-15 15:54:41 -0300

    2505151554 feat: modelo interno exemplo

---
+++ App/view/example.php
@@ -0,0 +1,8 @@
+<section class="jumbotron">
+        <div class="container">
+          <h1 class="jumbotron-heading">Example</h1>
+        </div>
+</section>
+<div class="container">
+lorem ipsum
+</div>
